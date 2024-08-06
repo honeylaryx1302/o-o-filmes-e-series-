5e732a308462f8d66abb6a1e21b36062c892f42f
honeylaryx1302 <162998971+honeylaryx1302@users.noreply.github.com>
2024-08-06 08:11:16 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -3,15 +3,15 @@ class Programa:
         self._nome = nome.title()
         self.ano = ano
         self._likes = 0
-    
+
     @property
     def nome(self):
         return self._nome
     
-    @nome.setter
-    def nome (self,novo_nome):
+    @nome.setter 
+    def nome (self, novo_nome):
         self._nome = novo_nome.title()
-    
+
     @property
     def likes(self):
         return self._likes
@@ -19,31 +19,45 @@ class Programa:
     def dar_likes(self):
         self._likes += 1
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self._likes}likes')
-
+    def __str__(self):
+     return f'{self.nome} - {self.ano} - {self._likes} likes'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.duracao = duracao
         self._likes = 0
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.temporadas = temporadas
         self._likes = 0
 
+class playlist(list):
+    def __init__(self,nome,programas):
+        self.nome = nome
+        super().__init__(programas)
+    
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta =Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em panico',1999, 100)
+demolidor = Serie('demolidor',2016, 2)
 
-vingadores = Filme('Vingadores - Guerra infinita', 2018, 160)
 vingadores.dar_likes()
-
-atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-filmes_e_series = [vingadores,atlanta]
+filmes_e_series = [vingadores, atlanta,demolidor,tmep]
+playlist_fim_de_semana = playlist('fim de semana',filmes_e_series)
 
-for programa in filmes_e_series:
-    programa.imprime()
+print(f'tamanho da playlist:{len(playlist_fim_de_semana)}')
+for programa in playlist_fim_de_semana:
+    print (programa)
+print(f'esta ou nao esta ?{demolidor in playlist_fim_de_semana}')
